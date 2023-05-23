63d517c39ac106f15e61d154af7c00b4e2f9416b
unknown <singh21.h@gmail.com>
2023-05-23 00:04:15 -0400

    Adding more features

--- index.js
+++ index.js
@@ -8,48 +8,44 @@ var app = express();
 app.use(express.static("public"));
 
 app.use(
-  express.urlencoded({
-    extended: true,
-  })
+	express.urlencoded({
+		extended: true,
+	})
 );
 
 // To tell express that we are going to use EJS
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  functions.list().then((result) => {
-    let array = [];
-    result.forEach((element) => {
-      array.push(element.name);
-    });
-    res.send(array);
-  });
+	functions.list().then((result) => {		
+		res.render("table", { array: result });
+	});
 });
 
 app.get("/about", function (req, res) {
-  res.send("This is the about page.");
+	res.render("about");
 });
 
 app.get("/ejs/:name", function (req, res) {
-  res.render("ejs", { data: req.params.name });
+	res.render("ejs", { data: req.params.name });
 });
 
 app.get("/form", function (req, res) {
-  res.render("serverless");
+	res.render("serverless");
 });
 
 app.post("/post_form", function (req, res) {
-  var code = req.body.code;
-  var answer = functions.create(code).then((result) => {
-    if (result === -1) {
-      res.send("An error occurred.");
-    } else {
-      res.render("ejs", {
-        link_name: result,
-        api_host: process.env.__OW_API_HOST,
-      });
-    }
-  });
+	var code = req.body.code;
+	var answer = functions.create(code).then((result) => {
+		if (result === -1) {
+			res.send("An error occurred.");
+		} else {
+			res.render("ejs", {
+				link_name: result,
+				api_host: process.env.__OW_API_HOST,
+			});
+		}
+	});
 });
 
 app.listen(process.env.PORT);
